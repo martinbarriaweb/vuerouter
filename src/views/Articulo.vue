4fd516e92d36f4036020b832fe7c3d8002ad5e9a
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-21 11:01:25 -0600

    feat(article)

--- src/views/Articulo.vue
+++ src/views/Articulo.vue
@@ -1,5 +1,7 @@
 <template>
   <Titulo txt="Articulo Pages" />
+  <div>{{item.title}}</div>
+  <div>{{item.body}}</div>
 </template>
 
 <script>
@@ -7,6 +9,28 @@ import Titulo from "../components/Titulo";
 export default {
   components: {
     Titulo,
+  },
+  methods: {
+    async consumeApi() {
+      try {
+        const data = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`
+        );
+        const itemTemp = await data.json();
+        this.item = itemTemp;
+        console.log(this.item);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  data() {
+    return {
+      item: [],
+    };
+  },
+  created() {
+    this.consumeApi();
   },
 };
 </script>
